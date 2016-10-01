dd2494c0fe4c3d48286f1b25fa120c5585b65ab5
Supersonic112 <supersonic112@live.de>
2016-10-01 15:06:54 +0200

    resized bar bgs, added central counter, updated bar positions

--- love2d/game_interface.lua
+++ love2d/game_interface.lua
@@ -1,11 +1,12 @@
 class "GameInterface" {
   fgBorderXOffset = 3,
   fgBorderYOffset = 3,
-  barHorizontalScreenOffset = 25,
+  barHorizontalScreenOffset = 36,
   barVerticalScreenOffset = 15
 }
 
 function GameInterface:__init()
+  -- The functions should return some value between 0 and 100
   self.waterFillStatus = function () return 100 end --TODO put function to get water status here
   self.fireFillStatus = function () return 75 end --TODO see above
   self.waterBGImage = love.graphics.newImage("gfx/ui-bar-bg-1.png")
@@ -14,27 +15,36 @@ function GameInterface:__init()
   self.fireBGImage = love.graphics.newImage("gfx/ui-bar-bg-0.png")
   self.fireFGImage = love.graphics.newImage("gfx/ui-bar-0.png")
   self.fireImage = love.graphics.newImage("gfx/symbol-0.png")
+  local imageFontSymbols = " abcdefghijklmnopqrstuvwxyz" ..
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ0" ..
+    "123456789.,!?-+/():;%&`'*#=[]\""
+  self.guiFont = love.graphics.newImageFont("data/default_imagefont.png", imageFontSymbols)
+  self.guiText = love.graphics.newText(self.guiFont, "test/test")
 end
 
 function GameInterface:draw()
-  self:drawPlayerBar(self.waterFillStatus(), 1)
-  self:drawPlayerBar(self.fireFillStatus(), 0)
-  --TODO draw info box in the middle
+  self:drawPlayerBars()
+  local gameStatus = {["saved"] = 85, ["existing"] = 100}
+  self:drawMiddleBar(gameStatus)
 end
 
--- fill_size = maybe between 0 and 100
--- side = 1 => water, side = 0 => fire
-function GameInterface:drawPlayerBar(fill_size, side)
-  --print(fill_size)
-  if side == 1 then
-    --water
-    love.graphics.draw(self.waterImage, self.barHorizontalScreenOffset-28, self.barVerticalScreenOffset-3)
-    love.graphics.draw(self.waterBGImage, self.barHorizontalScreenOffset, self.barVerticalScreenOffset)
-    love.graphics.draw(self.waterFGImage, self.barHorizontalScreenOffset+self.fgBorderXOffset, self.barVerticalScreenOffset+self.fgBorderYOffset, 0, 10*fill_size/100, 1)
-  else
-    --fire
-    love.graphics.draw(self.fireImage, love.graphics.getWidth()-self.barHorizontalScreenOffset-4, self.barVerticalScreenOffset-3)
-    love.graphics.draw(self.fireBGImage, love.graphics.getWidth()-self.barHorizontalScreenOffset, self.barVerticalScreenOffset, 0, -1.0, 1)
-    love.graphics.draw(self.fireFGImage, love.graphics.getWidth()-self.barHorizontalScreenOffset-self.fgBorderXOffset, self.barVerticalScreenOffset+self.fgBorderYOffset, 0, -1.0*10*fill_size/100, 1)
-  end
+function GameInterface:drawPlayerBars()
+  --water
+  love.graphics.draw(self.waterImage, self.barHorizontalScreenOffset-34, self.barVerticalScreenOffset-2)
+  love.graphics.draw(self.waterBGImage, self.barHorizontalScreenOffset, self.barVerticalScreenOffset)
+  -- self.waterFillStatus()/10 -> *10 for total size and /100 for value from fill status => /10
+  love.graphics.draw(self.waterFGImage, self.barHorizontalScreenOffset+self.fgBorderXOffset, self.barVerticalScreenOffset+self.fgBorderYOffset, 0, self.waterFillStatus()/10, 1)
+  --fire
+  love.graphics.draw(self.fireImage, love.graphics.getWidth()-self.barHorizontalScreenOffset+2, self.barVerticalScreenOffset-2)
+  love.graphics.draw(self.fireBGImage, love.graphics.getWidth()-self.barHorizontalScreenOffset, self.barVerticalScreenOffset, 0, -1.0, 1)
+  love.graphics.draw(self.fireFGImage, love.graphics.getWidth()-self.barHorizontalScreenOffset-self.fgBorderXOffset, self.barVerticalScreenOffset+self.fgBorderYOffset, 0, -1.0*self.fireFillStatus()/10, 1)
+end
+
+-- gameStatus = {["saved"] = 75, ["existing"] = 100}
+function GameInterface:drawMiddleBar(gameStatus)
+  self.guiText:set(gameStatus.saved.."/"..gameStatus.existing)
+  oldR, oldG, oldB = love.graphics.getColor()
+  love.graphics.setColor(255,255,255)
+  love.graphics.draw(self.guiText, love.graphics.getWidth()/2, self.barVerticalScreenOffset, 0, 2, 2, self.guiText:getWidth()/2, 0)
+  love.graphics.setColor(oldR, oldG, oldB)
 end
