e3e65c420466f5ccaab6f7fc25f52ef85a8aa178
Sebastian Dorda <hagish@schattenkind.net>
2016-10-01 15:02:33 +0200

    input shoot

--- love2d/player.lua
+++ love2d/player.lua
@@ -6,8 +6,9 @@ class "Player" {
 }
 
 cRotateSpeed = 0.85
-cCanonImpulse = 100
+cCanonImpulse = 40
 cShotRadius = 8
+cShootTimeout = 0.1
 
 function Player:__init(water, posx, posy, world)
 	self.water = water
@@ -16,6 +17,8 @@ function Player:__init(water, posx, posy, world)
 	self.world = world
 	self.inputRotateLeft = false
 	self.inputRotateRight = false
+	self.inputShoot = false
+	self.shootIdleTime = cShootTimeout
 	
 	if self.water then
 		self.image = love.graphics.newImage("gfx/car-1.png")
@@ -43,19 +46,26 @@ function Player:update(dt)
 	if self.inputRotateRight then self.rotate = self.rotate + cRotateSpeed end
 	
 	self.angle = self.angle + dt * self.rotate
+	
+	self.shootIdleTime = self.shootIdleTime + dt
+	
+	if self.inputShoot and self.shootIdleTime >= cShootTimeout then
+		self.shootIdleTime = 0
+		self:shoot()
+	end
 end
 
 function Player:draw()
-		love.graphics.setColor(255, 255, 255)
-		if self.water then
-			love.graphics.draw(self.image, self.quad, self.posx, self.posy)
-			love.graphics.draw(self.gunImage, self.gunQuad, self.posx - self.gunImage:getWidth()/2 + 20, self.posy + 30, 
-											self.angle, 1, 1, self.gunImage:getWidth()/2, self.gunImage:getHeight())
-		else
-			love.graphics.draw(self.image, self.quad, self.posx, self.posy)
-			love.graphics.draw(self.gunImage, self.gunQuad, self.posx + 20, self.posy + 25, 
-											self.angle, 1, 1, 3, self.gunImage:getHeight())
-		end
+	love.graphics.setColor(255, 255, 255)
+	if self.water then
+		love.graphics.draw(self.image, self.quad, self.posx, self.posy)
+		love.graphics.draw(self.gunImage, self.gunQuad, self.posx - self.gunImage:getWidth()/2 + 20, self.posy + 30, 
+										self.angle, 1, 1, self.gunImage:getWidth()/2, self.gunImage:getHeight())
+	else
+		love.graphics.draw(self.image, self.quad, self.posx, self.posy)
+		love.graphics.draw(self.gunImage, self.gunQuad, self.posx + 20, self.posy + 25, 
+										self.angle, 1, 1, 3, self.gunImage:getHeight())
+	end
 end
 
 function Player:keyreleased(key)
@@ -64,6 +74,9 @@ function Player:keyreleased(key)
 	end
 	if key == self.keyRight then
 		self.inputRotateRight = false
+	end
+	if key == self.keyShoot then
+		self.inputShoot = false
 	end
 end
 
@@ -88,6 +101,6 @@ function Player:keypressed(key, scancode, isrepeat)
 		self.inputRotateRight = true
 	end
 	if key == self.keyShoot then
-		self:shoot()
+		self.inputShoot = true
 	end
 end
